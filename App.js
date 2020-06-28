3d1dea248e94f2b4bfdd398ebdba5724e9139ce8
Kristy Li <kristyli20.0@gmail.com>
2020-06-28 11:55:07 -0400

    Update App component to persist state and allow for navigation

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator} from '@react-navigation/stack';
+import React, { useState } from "react";
+import { StyleSheet, Text, ScrollView, View, FlatList } from "react-native";
+import Home from './screens/Home';
+import AuthForm from './screens/AuthForm';
+import Manual from './screens/Manual';
+import Challenges from './screens/Challenges';
+import {Provider} from 'react-redux';
+import { createStore, applyMiddleware} from 'redux';
+import rootReducer from './redux/reducers';
+import AsyncStorage from '@react-native-community/async-storage';
+import {persistStore, persistReducer} from 'redux-persist';
+import {createLogger} from 'redux-logger';
+import { PersistGate } from 'redux-persist/integration/react';
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whiteList: ['statusReducer']
+};
+
+const Stack = createStackNavigator();
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+const store = createStore(
+  persistedReducer, applyMiddleware(createLogger())
+);
+const persistedStore = persistStore(store);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <Provider store={store}>
+      <PersistGate persistor={persistedStore}>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen name="Challenges" component={Challenges} />
+            <Stack.Screen name="Login" component={AuthForm} />
+            <Stack.Screen name="Register" component={AuthForm} />
+            <Stack.Screen name="Manual" component={Manual} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PersistGate>
+    </Provider>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
