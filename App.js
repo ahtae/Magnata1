d5139622cd8d4b5cef79d6f07e4a154f90e47cf7
Kristy Li <kristyli20.0@gmail.com>
2020-06-28 17:24:41 -0400

    Update App component to allow the state to persist and have additional navigation screens

--- App.js
+++ App.js
@@ -1,31 +1,42 @@
-import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator} from '@react-navigation/stack';
-import React, { useState } from "react";
-import { StyleSheet, Text, ScrollView, View, FlatList } from "react-native";
-import Home from './screens/Home';
-import AuthForm from './screens/AuthForm';
-import Manual from './screens/Manual';
-import Challenges from './screens/Challenges';
-import {Provider} from 'react-redux';
-import { createStore, applyMiddleware} from 'redux';
-import rootReducer from './redux/reducers';
-import AsyncStorage from '@react-native-community/async-storage';
-import {persistStore, persistReducer} from 'redux-persist';
-import {createLogger} from 'redux-logger';
-import { PersistGate } from 'redux-persist/integration/react';
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Home from "./screens/Home";
+import Manual from "./screens/Manual";
+import LoginForm from "./screens/LoginForm";
+import Challenges from "./screens/Challenges";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import rootReducer from "./redux/reducers";
+import AsyncStorage from "@react-native-community/async-storage";
+import { persistStore, persistReducer } from "redux-persist";
+import { createLogger } from "redux-logger";
+import { PersistGate } from "redux-persist/integration/react";
+import * as firebase from "firebase";
+import RegisterForm from "./screens/RegisterForm";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAv49P7QArAi4yFT4Ir0fswrz-g2v1oQRE",
+  authDomain: "magnata-ba19a.firebaseapp.com",
+  databaseURL: "https://magnata-ba19a.firebaseio.com",
+  projectId: "magnata-ba19a",
+  storageBucket: "magnata-ba19a.appspot.com",
+  messagingSenderId: "660843609937",
+  appId: "1:660843609937:web:4e48e2cc9dce29b276dd70",
+  measurementId: "G-KFMDXNDWMW",
+};
+
+firebase.initializeApp(firebaseConfig);
 
 const persistConfig = {
-  key: 'root',
+  key: "root",
   storage: AsyncStorage,
-  whiteList: ['statusReducer']
+  whiteList: ["statusReducer", "navigation"],
 };
 
 const Stack = createStackNavigator();
 const persistedReducer = persistReducer(persistConfig, rootReducer);
-const store = createStore(
-  persistedReducer, applyMiddleware(createLogger())
-);
+const store = createStore(persistedReducer, applyMiddleware(createLogger()));
 const persistedStore = persistStore(store);
 
 export default function App() {
@@ -35,13 +46,13 @@ export default function App() {
         <NavigationContainer>
           <Stack.Navigator>
             <Stack.Screen name="Home" component={Home} />
-            <Stack.Screen name="Challenges" component={Challenges} />
-            <Stack.Screen name="Login" component={AuthForm} />
-            <Stack.Screen name="Register" component={AuthForm} />
+            <Stack.Screen name="Login" component={LoginForm} />
+            <Stack.Screen name="Register" component={RegisterForm} />
             <Stack.Screen name="Manual" component={Manual} />
+            <Stack.Screen name="Challenges" component={Challenges} />
           </Stack.Navigator>
         </NavigationContainer>
       </PersistGate>
     </Provider>
-  )
+  );
 }
